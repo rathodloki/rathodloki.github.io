1ce22e35171a34c9980de7853c83d67cc178e199
Lokendar Singh <odl2s@odqad.com>
2022-02-25 02:35:06 +0530

    Dark mode added

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var dark=true
+	$(".nav-menu li:nth-child(3)").click(function(){
+	if(dark){
+	$('link[href="assets/css/style.css"]').attr('href','assets/css/style2.css');
+	$(".nav-menu li:nth-child(3) a svg").attr("data-icon","ic:outline-dark-mode");
+	$(".nav-menu li:nth-child(3) a  span").text("Dark");
+	dark = false;
+	}
+	else{
+	$('link[href="assets/css/style2.css"]').attr('href','assets/css/style.css');
+	$(".nav-menu li:nth-child(3) a svg").attr("data-icon","ic:outline-light-mode");
+	$(".nav-menu li:nth-child(3) a span").text("Light");
+	dark = true;
+	}
+	})
+});
